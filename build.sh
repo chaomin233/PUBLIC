984ca0706e9e35a3173ca49d98c4ddb030f4bfb5
chaomin233 <53070297+chaomin233@users.noreply.github.com>
2019-07-25 20:24:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-set -e
-
 cd `dirname $0`
 echo "检查yarn"
+yarn -v &> /dev/null
 
-if ! which yarn -v &> /dev/null ;then
-  echo "安装yarn"
-  npm i -g yarn
+if [[ ! $? -eq 0 ]]
+then
+    echo "安装yarn"
+    npm i -g yarn
 fi
 
 echo "build project"
 yarn --frozen-lockfile && yarn build
 
 echo "压缩项目"
-cd dist
-tar -czf ../$APP_NAME-v$VERSION.tar.gz .
-cd ..
+tar -cf $APP_NAME-v$VERSION.tar.gz ./dist
